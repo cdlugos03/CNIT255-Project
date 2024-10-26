bfc628ae6ed5e1e75dd54aab836eb26d1f5d17fb
cdlugos03 <113647528+cdlugos03@users.noreply.github.com>
2024-10-26 13:32:31 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+// CNIT25501_PROJECT -- Main.java
+// Class Function: This class serves as the entry point for the BlackJack game.
+// It creates a BlackJack game instance, which initializes the game and user interface.
+
 public class Main {
+     //Main method: Entry point of the program. It creates a new BlackJack game instance.
     public static void main(String[] args) {
         BlackJack blackjack = new BlackJack();
-
     }
 }
